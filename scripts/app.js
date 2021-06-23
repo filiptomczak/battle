3400728e19d7abaff42eb33b99a6b7e9370a2c67
Kornel <flp.tomczak@gmail.com>
2021-06-23 12:00:50 +0200

    improved styles, add logic to heal and log btns

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,56 @@
 const PLAYER_ATTACK=10;
 const PLAYER_STRONG_ATTACK=20;
 const MONSTER_ATTACK=15;
+const PLAYER_HEAL=10;
 
-const PLAYER_INITIAL_HP=prompt("set your initial hp");
-const MONSTER_INITIAL_HP=prompt("set monster initial hp");
+let logHistory=[];
 
-init(PLAYER_INITIAL_HP,MONSTER_INITIAL_HP);
-
+init();
+function onLog(){
+    console.log(logHistory);
+}
+function writeToLog(text,value){
+    log={
+        text:text,
+        value:value
+    }
+    logHistory.push(log);
+}
+function onHeal(){
+    const healValue=handlePlayerHeal(PLAYER_HEAL);
+    writeToLog("HEAL",healValue);
+}
 function onAttack(){
-    handlePlayerAttack(PLAYER_ATTACK);
+    const attackValue=handlePlayerAttack(PLAYER_ATTACK);
+    writeToLog("ATTACK",attackValue);
+    endRound();
 }
 function onStrong(){
-    handlePlayerAttack(PLAYER_STRONG_ATTACK);
+    const attackValue=handlePlayerAttack(PLAYER_STRONG_ATTACK);
+    writeToLog("STRONG_ATTACK",attackValue);
+    endRound();
 }
 function endRound(){
-    monsterAttack();
-    let currentMonsterHP=monsterHealthBar.value;
-    let currentPlayerHP=playerHealthBar.value;
+    const attackValue=monsterAttack();
+    writeToLog("MONSTER_ATTACK",attackValue);
+
+    let currentMonsterHP=progressMonster.value;
+    let currentPlayerHP=progressPlayer.value;
 
     if(currentMonsterHP<=0){
         alert("PLAYER WON!");
+        writeToLog("PLAYER WON!");
+        reset();
     }else if(currentPlayerHP<=0){
         alert("MONSTER WON!");
-    }else{
-        return;
+        writeToLog("MONSTER WON!");
+        reset();
     }
 }
 function monsterAttack(){
-    handleMonsterAttack(monsterAttack);
+    handleMonsterAttack(MONSTER_ATTACK);
 }
 btnAttack.addEventListener('click',onAttack);
 btnStrongAttack.addEventListener('click',onStrong);
+btnHeal.addEventListener('click',onHeal);
+btnLog.addEventListener('click',onLog);
--- scripts/vendor.js
+++ scripts/vendor.js
@@ -1,42 +1,61 @@
 console.log("loaded vendor.js");
 let INITIALIZE=true;
-
+let PLAYER_INITIAL_HP=100;
+let MONSTER_INITIAL_HP=100;
+let HEAL_COUNTER=0;
 
 const btnAttack=document.getElementById("attackBtn");
 const btnStrongAttack=document.getElementById("strongAttackBtn");
-const btnHeal=document.getElementById("healkBtn");
-const btnLog=document.getElementById("logkBtn");
+const btnHeal=document.getElementById("healBtn");
+const btnLog=document.getElementById("logBtn");
 
 const progressPlayer=document.getElementById("playerHealth");
 const progressMonster=document.getElementById("monsterHealth");
 
-function init(playerHP,monsterHP){
+function init(){
     if(INITIALIZE){
-        const PLAYER_INITIAL_HP=prompt("set your initial hp");
-        const MONSTER_INITIAL_HP=prompt("set monster initial hp");
+        PLAYER_INITIAL_HP=prompt("set your initial hp");
+        MONSTER_INITIAL_HP=prompt("set monster initial hp");
+        HEAL_COUNTER=0;
+        btnHeal.classList.remove("deactivateBtn");
+        if(isNaN(PLAYER_INITIAL_HP)||PLAYER_INITIAL_HP<=0) PLAYER_INITIAL_HP=100;
+        if(isNaN(MONSTER_INITIAL_HP)||MONSTER_INITIAL_HP<=0) MONSTER_INITIAL_HP=100;
 
-        if(isNaN(playerHP)||playerHP<=0) playerHP=100;
-        if(isNaN(monsterHP)||monsterHP<=0) monsterHP=100;
-
-        progressPlayer.value=playerHP;
-        progressPlayer.max=playerHP;
-        progressMonster.value=monsterHP;
-        progressMonster.max=monsterHP;
+        progressPlayer.value=PLAYER_INITIAL_HP;
+        progressPlayer.max=PLAYER_INITIAL_HP;
+        progressMonster.value=MONSTER_INITIAL_HP;
+        progressMonster.max=MONSTER_INITIAL_HP;
     }
     INITIALIZE=false;
 }
 function handlePlayerAttack(attackValue){
-    attackValue=(Math.floor(Math.random()*attackValue)+1);
+    attackValue=Math.floor(Math.random()*attackValue)+1;
     progressMonster.value-=attackValue;
-    if(progressMonster.value<=0.3*MONSTER_INITIAL_HP)
-    console.log(progressMonster.value);
+}
+function handlePlayerHeal(healValue){
+    if(HEAL_COUNTER<4){
+        healValue=Math.floor(Math.random()*healValue)+1;
+        progressPlayer.value+=healValue;
+        HEAL_COUNTER++;
+    }else{
+        btnHeal.classList.add("deactivateBtn");
+    }
 }
 function handleMonsterAttack(attackValue){
-    attackValue=(Math.floor(Math.random()*attackValue)+1);
+    attackValue=Math.floor(Math.random()*attackValue)+1;
     progressPlayer.value-=attackValue;
-    console.log(progressPlayer.value);
+    checkHP(progressPlayer.value);
+}
+function checkHP(hp){
+    if(hp<0.3*PLAYER_INITIAL_HP){
+        progressPlayer.classList.add("redHP");
+    }else{
+        progressPlayer.classList.remove("redHP");
+    }
 }
 function reset(){
     INITIALIZE=true;
-    init
+
+    
+    init();
 }
